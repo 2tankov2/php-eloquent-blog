473cde31532ac04f882735eef825ae0251868921
tankov <tan.kov@hotmail.com>
2020-03-09 23:31:34 +0300

    work with collect

--- src/actions/Posts.php
+++ src/actions/Posts.php
@@ -26,4 +26,17 @@ class Posts
         return $like;
         // END
     }
+
+    public static function index($user, $limit)
+    {
+        $posts = Post::limit($limit)->orderBy('created_at', 'desc')->get();
+        $postIds = $posts->pluck('id');
+        $likedPostIds = $user->postLikes()->whereIn('post_id', $postIds)->pluck('post_id');
+
+        $result = $posts->map(function ($post) use ($likedPostIds) {
+            return ['post' => $post->toArray(), 'liked' => $likedPostIds->contains($post->id)];
+        });
+
+        return $result;
+    }
 }
